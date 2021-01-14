47b4216f823ea491c287e2400330cbc93644a3de
mateusdeitos <mate.deitos@hotmail.com>
2021-01-14 15:16:07 -0300

    Ajustes features desnecessarias

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable react/no-children-prop */
 /* eslint-disable no-restricted-globals */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-import AppProvider from './hooks/index';
 import useColorMode from './hooks/useColorMode';
 
 const App = () => {
   const [colorMode] = useColorMode();
   return (
     <Router>
-      <AppProvider>
-        <Routes />
-      </AppProvider>
+      <Routes />
       <GlobalStyle theme={colorMode} />
     </Router>
   );

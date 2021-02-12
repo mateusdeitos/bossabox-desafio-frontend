2709541add08f22199f6c25c2d809f7bdf68f40c
Mateus Campos Deitos <mate.deitos@hotmail.com>
2021-02-12 13:01:41 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    authorization: `Bearer ${process.env.API_TOKEN}`,
+    authorization: process.env.REACT_APP_API_TOKEN,
   },
 });
 
